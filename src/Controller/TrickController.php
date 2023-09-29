4210fc8764d8af895076d6c862fd561ad0a9295d
Coralie <coralie.burtin@protonmail.com>
2023-09-29 09:27:56 +0200

    fix after review

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -2,8 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Trick;
 use App\Repository\GroupRepository;
-use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,24 +12,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class TrickController extends AbstractController
 {
     #[Route('/trick/{slug}', name: 'app_trick')]
-    public function index(string $slug, Request $request, TrickRepository $trickRepository, GroupRepository $groupRepository): Response
+    public function index(Request $request, Trick $trick): Response
     {
-        $trick = $trickRepository->findOneBy(['slug' => $slug]);
-        $groups = $groupRepository->findAll();
-
         $frameId = $request->headers->get('Turbo-Frame');
 
         return $this->render('trick/index.html.twig', [
             'trick' => $trick,
-            'groups' => $groups,
             'isModal' => $frameId ? true : false,
         ]);
     }
 
     #[Route('/trick/{slug}/edit', name: 'app_trick_edit')]
-    public function edit(string $slug,Request $request, TrickRepository $trickRepository, GroupRepository $groupRepository): Response
+    public function edit(Request $request, Trick $trick, GroupRepository $groupRepository): Response
     {
-        $trick = $trickRepository->findOneBy(['slug' => $slug]);
         $groups = $groupRepository->findAll();
 
         $frameId = $request->headers->get('Turbo-Frame');
