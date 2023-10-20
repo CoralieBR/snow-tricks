3c7e6eb469e15f05e6719d1c8e1114f70535a104
Coralie <coralie.burtin@protonmail.com>
2023-10-20 10:15:38 +0200

    turbo stream templates

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -11,11 +11,12 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\UX\Turbo\TurboBundle;
 
 class TrickController extends AbstractController
 {
     #[Route('/trick/ajouter', name: 'app_trick_new')]
-    public function new(Request $request, SluggerInterface $slugger, EntityManagerInterface $em): Response
+    public function new(Request $request, SluggerInterface $slugger, EntityManagerInterface $em)
     {
         $trick = new Trick();
         $form = $this->createForm(TrickType::class, $trick, [
@@ -30,25 +31,72 @@ class TrickController extends AbstractController
             $em->persist($trick);
             $em->flush();
 
-            return $this->redirectToRoute('app_homepage', [], 303);
+            return $this->redirectToRoute('app_trick', ['slug' => $trick->getSlug()]);
         }
 
-        $frameId = $request->headers->get('Turbo-Frame');
-        return $this->render('trick/new.html.twig', [
-            'isModal' => $frameId ? true : false,
-            'form' => $form
-        ]);
+        try {
+            $request->setRequestFormat(TurboBundle::STREAM_FORMAT);
+            return $this->render('trick/stream/_modal.stream.html.twig', [
+                'action' => 'new',
+                'form' => $form,
+            ]);
+        } catch (\Throwable $th) {
+            return $this->render('trick/new.html.twig', [
+                'form' => $form,
+            ]);
+        }
+    }
+
+    #[Route('/trick/fermer', name:'app_trick_modal_close')]
+    public function close(Request $request)
+    {
+        try {
+            $request->setRequestFormat(TurboBundle::STREAM_FORMAT);
+            return $this->render('trick/stream/close-modal.stream.html.twig');
+        } catch (\Throwable $th) {
+            return $this->redirectToRoute('app_homepage');
+        }
+    }
+
+    #[Route('/trick/supprimer/{slug}', name: 'app_trick_delete')]
+    public function delete(Trick $trick, Request $request, EntityManagerInterface $em)
+    {
+        $id = $trick->getId();
+        $name = $trick->getName();
+        $em->remove($trick);
+        $em->flush();
+
+        try {
+            $request->setRequestFormat(TurboBundle::STREAM_FORMAT);
+            return $this->render('trick/stream/deleted.stream.html.twig', [
+                'id' => $id,
+                'name' => $name,
+            ]);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 
     #[Route('/trick/{slug}', name: 'app_trick')]
     public function index(Request $request, Trick $trick): Response
     {
-        $frameId = $request->headers->get('Turbo-Frame');
-
-        return $this->render('trick/index.html.twig', [
+        $params = [
             'trick' => $trick,
-            'isModal' => $frameId ? true : false,
-        ]);
+            'action' => 'show',
+        ];
+        if ($request->query->get('after-edition')) {
+            $params['turbo-action'] = 'update';
+            $params['turbo-target'] = 'trick-modal';
+        }
+        try {
+            $request->setRequestFormat(TurboBundle::STREAM_FORMAT);
+            return $this->render('trick/stream/_modal.stream.html.twig', $params);
+        } catch (\Throwable $th) {
+            return $this->render('trick/show.html.twig', [
+                'trick' => $trick,
+                'isModal' => false,
+            ]);
+        }
     }
 
     #[Route('/trick/{slug}/edit', name: 'app_trick_edit')]
@@ -64,15 +112,25 @@ class TrickController extends AbstractController
 
             $em->flush();
 
-            return $this->redirectToRoute('app_trick', ['slug' => $trick->getSlug()], 303);
+            return $this->redirectToRoute('app_trick', [
+                'slug' => $trick->getSlug(),
+                'after-edition' => true,
+            ], 303);
         }
 
-        $frameId = $request->headers->get('Turbo-Frame');
-        return $this->render('trick/index.html.twig', [
-            'trick' => $trick,
-            'isModal' => $frameId ? true : false,
-            'edit' => true,
-            'form' => $form
-        ]);
+        try {
+            $request->setRequestFormat(TurboBundle::STREAM_FORMAT);
+            return $this->render('trick/stream/_modal.stream.html.twig', [
+                'trick' => $trick,
+                'form' => $form,
+                'action' => 'edit',
+            ]);
+        } catch (\Throwable $th) {
+            return $this->render('trick/edit.html.twig', [
+                'trick' => $trick,
+                'isModal' => false,
+                'form' => $form
+            ]);
+        }
     }
 }
